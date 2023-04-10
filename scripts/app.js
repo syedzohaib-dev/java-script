315f054da66f1cb5bc9383595e26e0ad1f1f683a
syed zohaib <zohaibsyed914@gmail.com>
2023-04-10 13:59:05 -0700

    make variable

---
+++ scripts/app.js
@@ -0,0 +1,67 @@
+// alert("hello world")
+// alert('Hello World')
+// var username =prompt("Your Name PLease","Usama")
+// var user = "Usama"
+// var fuser = "Usman"
+// console.log("Hello " + user+" " +fuser)
+
+
+
+// variable
+
+// console.log("this is a javascript code 01")
+// let a = 23
+// document.write(a)
+// a = "harry"
+// console.log(a)
+
+// var username = prompt("username bataoo")
+// document.write( "hello " + username + " kese ho")
+
+// var halchal = "kese ho"
+// var age = 19
+// console.log(typeof age)
+// console.log(typeof halchal)
+
+// var age = +prompt("your age please")
+// var fage = +prompt("your fage please")
+// // console.log(Number(fage)+Number(age)
+// console.log(age+fage)
+
+// task 1.......zakat.....
+
+// var zakatpercentage = 0.025
+// var userinput = +prompt("write your zakat contity")
+// var result = (zakatpercentage*userinput)
+// alert("your zakat value is "+result)
+
+// console.log(20+2)
+// console.log(20*2)
+// console.log(20-2)
+// console.log(20/2)
+// console.log(2**2)
+
+// product card.......
+
+// var num = 1
+// console.log(num)
+// // num = num*1
+// num +=1
+// console.log(num)
+
+// // console.log(++num)...pre...
+// // console.log(num++)...post...
+
+
+// console.log(((1+8)*(5/8))+((7-34)/(8+9)))
+
+
+
+
+
+
+
+
+
+
+
