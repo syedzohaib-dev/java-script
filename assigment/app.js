2520327f3dc419500d9df6a8daf4d78722bffff1
syed zohaib <zohaibsyed914@gmail.com>
2023-04-21 11:53:26 -0700

    task

--- assigment/app.js
+++ assigment/app.js
@@ -8,8 +8,98 @@
 
 // task 2.........fitra......
 
-var fitrarates = ('aata 320', 'khajoor 2800', 'kishmish 6400', 'jo 480')
-var famitymember = +prompt("apne kitne logo ka fitra dena he")
-var userinput = prompt("apko konsa fitrah dena he")
-var result = (famitymember*userinput)
-alert("your fitra is" + result)
+
+// var famitymember = prompt("please type total number of family");
+// var fitratype = prompt("please find your fithra: 1. barley (480), 2. dates (2800), 3. wheat-flour (320), 4. raisins (6400)");
+// if (fitratype === "1") {
+//     fitrarate = 480
+// } else if (fitratype === "2") {
+//     fitrarate = 2800
+// } else if (fitratype === "3") {
+//     fitrarate = 320
+// } else if (fitratype === "4") {
+//     fitrarate = 6400
+// }
+// var result = (familymember * fitrarate);
+// alert("your total fitra is" + result);
+
+
+// task 3......guess the number...
+
+
+// var userinput = prompt("guess the number for (1 to 10)");
+// if (userinput == 3) {
+//     alert("you win");
+// }
+// else if (userinput >= 3) {
+//     alert("you lose");
+// }
+// else if (userinput <= 3) {
+//     alert("you lose");
+// }
+
+
+// task 4......capital format......
+
+
+// var userinput = prompt("your name please")
+// alert("first letter of your name is capital " + userinput[0].toUpperCase() + userinput.toLocaleLowerCase())
+
+
+// task 5.....array...name and num...
+
+// var contactnumber = []
+// var contactname = []
+// for (var i = 0; i < 4; i++) {
+//     var contactnumber = +prompt("contact number")
+//     var contactname = +prompt("contactname")
+//     contactnumber.push(contactnumber)
+//     contactname.push(contactname)
+// }
+// console.log("contact number")
+// for (let i = 0; < contactnumber.length; i++) {
+//     console.log(contactnumber[i])
+// }
+// console.log("contactname")
+// for (let i = 0; i < contactname.length; i++) {
+//     console.log(contactname[i])
+// }
+
+// task 6.....product....
+
+// var product = ['shoes', 't-shirt', 'pants', 'had', 'socks', 'tai', 'glases']
+// var position = prompt("chose the item")
+// position = parseint(position)
+// var removeitem = product.splice(position, 1)[0]
+// console.log("remove item" + removeitem)
+// console.log("remaining item" + product.join(", "))
+// console.log("total number of remaining item is" + product.lenth)
+
+
+// task 7....nationality...gender...age...
+
+
+// var nationality = prompt("your nationality");
+// var gender = prompt("your gender");
+// var age = prompt("your age");
+// if ((nationality === "Pakistani" || nationality === "pakistani" || nationality === "pak")) {
+//     if (gender === "male" && age >= 18) {
+//         alert("go to the voting room");
+//     }
+//     else if (gender === "female" && age >= 18) {
+//         let married = prompt("Are you married (yes/no)");
+//         if (married === "yes") {
+//             alert("You are go to vote.");
+//         }
+//         else {
+//             alert("You are not eligible to vote.");
+//         }
+//     }
+//     else {
+//         alert("You not eligible to vote.");
+//     }
+// } else {
+//     alert("You not eligible to vote.");
+// }
+
+// task 8........
